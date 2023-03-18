54c93056f7e3beb99b34c3272b6d91ab67e865cf
EnsasFasters <120198765+EnsasFasters@users.noreply.github.com>
2023-03-18 05:12:07 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
 $(document).ready(function(){
-	$('header').fadeIn(1000);
-	$('main').fadeIn(2000);
+	$('main').fadeIn(3000);
+})
+
+var pixelDelay;
+function scrollToElement(id){
+	var element = document.getElementById(id);
+	var offsetTop = element.offsetTop;
+	offsetTop-=pixelDelay;
+	window.scrollTo({
+		top:offsetTop
+	});
+	hideBar();
+}
+
+
+
+function handleMediaQueryChange(event) {
+  if (event.matches) {
+      pixelDelay = -100;
+  } else {
+      pixelDelay = 40;  
+  }
+      console.log(pixelDelay);
+}
+
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+mediaQuery.addEventListener('change', handleMediaQueryChange);
+handleMediaQueryChange(mediaQuery);
+
+
+/*NavBar*/
+
+let nav_bar = false;
+
+const $header  = $('header');
+const $body    = $('body');
+const $button  = $('body > div > div');
+
+$button.click(function() {
+  nav_bar ? hideBar() : showBar();
 });
+
+function hideBar() {
+  $header.css('right', '100vw');
+  $body.css('overflow-y', 'visible');
+  nav_bar = !nav_bar;
+}
+
+function showBar() {
+  $header.css('right', '0');
+  $body.css('overflow-y', 'hidden');
+  nav_bar = !nav_bar;
+}
